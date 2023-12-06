3d8f41a00874cc0ccbd752c3fe01c7fcc08d730d
shlomitub28 <shlomitub28@hotmail.com>
2023-12-06 11:27:42 +0200

    new commit

---
+++ entry.py
@@ -0,0 +1,16 @@
+# Databricks Python Task
+def main():
+    # Your Python code here
+
+    # Install libraries from requirements.txt in the Git repository
+    %pip install -r requirements.txt
+
+    # Continue with your code
+    import pandas as pd
+    import numpy as np
+
+    df = pd.DataFrame({"column1": [1, 2, 3], "column2": [4, 5, 6]})
+    print(df.head())
+
+if __name__ == "__main__":
+    main()
