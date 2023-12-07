27fb4a82fb168160490fd8ba5375ac221236b5a1
shlomitub28 <shlomitub28@hotmail.com>
2023-12-07 10:14:08 +0200

    added level1

--- level1/level1.py
+++ level1/level1.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
-subprocess.call([sys.executable, '-r ../requirements.txt', 'pip', 'install'])    
+import os
+os.chdir("../")
+subprocess.call([sys.executable, '-r requirements.txt', 'pip', 'install'])    
 import camelcase
 c = camelcase.CamelCase()
 txt = "hello world"
